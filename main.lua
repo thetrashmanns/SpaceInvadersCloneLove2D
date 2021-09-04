5e41b47f638e2dee7ce7638bb59f8f312983b900
LoganConnolly <77025699+LoganConnolly@users.noreply.github.com>
2021-09-04 10:15:16 -0700

    License Stuff for LoveFrames

--- main.lua
+++ main.lua
@@ -3,6 +3,27 @@ A Space Invaders Clone by Logan Connolly
 Made using Love2D 11.3 and LoveFrames 11.3
 And before we start this makes heavy use of LoveFrame's
 state system
+Copyright (c) 2014 Kenny Shields
+
+This software is provided 'as-is', without any express or implied warranty.
+In no event will the authors be held liable for any damages arising from
+the use of this software.
+
+Permission is granted to anyone to use this software for any purpose,
+including commercial applications, and to alter it and redistribute it
+freely, subject to the following restrictions:
+
+1. The origin of this software must not be misrepresented; you must not
+claim that you wrote the original software. If you use this software in a
+product, an acknowledgment in the product documentation would be
+appreciated but is not required.
+
+2. Altered source versions must be plainly marked as such, and must not
+be misrepresented as being the original software.
+
+3. This notice may not be removed or altered from any source
+distribution.
+^LoveFrames Stuff
 ]]
   function love.load()
     loveframes = require("gui.loveframes")
